0f49c2781a07e3a6b909a7e322c91b8591fb2685
bitzelcrafpt <138144560+bitzelcrafpt@users.noreply.github.com>
2025-06-30 18:17:52 +0000

    Add files via upload

---
+++ ModInfo.cs
@@ -0,0 +1,9 @@
+namespace PortaBoard
+{
+    public class ModInfo
+    {
+        public const string VERSION = "1.0.0";
+        public const string NAME = "PortaBoard";
+        public const string AUTHOR = "bitzelcrafpt";
+    }
+}
